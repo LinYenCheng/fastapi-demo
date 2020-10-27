f90898c959c3fcfcec0ee4ed88cadaacbf4d6631
tokikanno <toki.kanno@gmail.com>
2020-10-27 10:40:34 +0000

    refine demo contents

--- demo.py
+++ demo.py
@@ -64,6 +64,44 @@ def get_book_by_id_with_validation_and_some_extra_documnet(
         'book_id': book_id
     }
 
+# --------------------------------------------------
+# How to describe your response
+# --------------------------------------------------
+from pydantic import BaseModel, Field
+
+class BookCategory(str, Enum):
+    comics = 'comics'
+    cooking = 'cooking'
+    
+class Book(BaseModel):
+    bid: int = Field(..., ge=1, title='book id', description='`markdown`', example=5)
+    name: str = Field(..., min_length=2)
+    price: float = Field(..., gt=0)
+    category: BookCategory
+
+@app.get('/book/{book_id}/with_response_model', response_model=Book)
+def get_book_by_id_with_validation_and_some_extra_documnet(
+    book_id: int = Path(..., ge=1, example=5)
+):
+    # return {'book_id': book_id}
+
+    return {
+        'bid': book_id,
+        'name': 'name of bid {}'.format(book_id),
+        'price': 39.95,
+        'category': 'cooking'
+    }
+
+
+# --------------------------------------------------
+# Can we use BaseModel object as input? 
+# --------------------------------------------------
+@app.post('/book', response_model=Book)
+def get_book_by_id_with_validation_and_some_extra_documnet(
+    payload: Book
+):
+    payload.name += ' suffix'
+    return payload
 
 # --------------------------------------------------
 # Redirect home to docs
